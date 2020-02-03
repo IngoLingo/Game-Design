ea5ac9cf38a308de1645ce399f510ba98942311f
Gee 3.0 <n.ethington@hotmail.com>
2020-02-02 22:20:02 -0700

    stupid collision fixed

---
+++ Virus Game for Game Dev/Assets/Scripts/ChangeColorScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorScript : MonoBehaviour
+{
+    void Start() 
+    {
+        Debug.Log("START!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().material.color = new Color(0.5f,1,0.5f);
+        Debug.Log("It Works!");
+    }
+}
--- Virus Game for Game Dev/Assets/Scripts/Movement Script.cs
+++ Virus Game for Game Dev/Assets/Scripts/Movement Script.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeColorScript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
